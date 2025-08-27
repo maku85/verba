6d0ae5d30ca929fafd936084c77e5c09d4336e80
Maku <makuma85@gmail.com>
2025-08-27 20:55:29 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 const withNextIntl = createNextIntlPlugin();
